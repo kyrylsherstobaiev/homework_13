dff7430699603548766c4c78f483da6247ced8aa
kyrylsherstobaiev <novemberua@gmail.com>
2021-03-13 21:08:57 +0200

    correct

--- homework13_2/script.js
+++ homework13_2/script.js
@@ -2,9 +2,11 @@ let inputs = document.querySelectorAll('input[data-text="text"]');
 let textarea = document.querySelector('textarea');
 let inputArray = [];
 
+//***************************************************************/
+
 inputs.forEach(function (input) {
-	input.addEventListener('change', getValueInput)
-	input.addEventListener('focus', getClearValue)
+	input.addEventListener('change', getValueInput);
+	input.addEventListener('focus', getClearValue);
 });
 
 function getClearValue() {
@@ -12,8 +14,51 @@ function getClearValue() {
 }
 
 function getValueInput() {
-	inputArray.push(this.value);
+	switch (this) {
+		case document.querySelector('input[data-text="text"]:first-of-type'):
+			inputArray[0] = this.value;
+			break;
+		case document.querySelector('input[data-text="text"]:nth-of-type(2)'):
+			inputArray[1] = this.value;
+			break;
+		case document.querySelector('input[data-text="text"]:last-of-type'):
+			inputArray[2] = this.value;
+			break;
+	}
 	textarea.innerHTML = inputArray.join(',');
 }
 
-console.dir(inputArray);
+//************************************************************** */
+
+
+//function getValueInput() {
+//	if (this == document.querySelector('input[data-text="text"]:first-of-type')) {
+//		inputArray[0] = this.value;
+//	}
+//	if (this == document.querySelector('input[data-text="text"]:nth-of-type(2)')) {
+//		inputArray[1] = this.value;
+//	}
+//	if (this == document.querySelector('input[data-text="text"]:last-of-type')) {
+//		inputArray[2] = this.value;
+//	}
+//	textarea.innerHTML = inputArray.join(',');
+//	console.log(inputArray);
+//}
+
+//***************************************************************/
+
+//inputs.forEach(function (input) {
+//	input.addEventListener('change', getValueInput)
+//	input.addEventListener('focus', getClearValue)
+//});
+
+//function getClearValue() {
+//	this.value = '';
+//}
+
+//function getValueInput() {
+//	inputArray.push(this.value);
+//	textarea.innerHTML = inputArray.join(',');
+//}
+
+//console.dir(inputArray);

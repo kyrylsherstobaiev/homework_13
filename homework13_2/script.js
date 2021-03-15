c43568f723bcc5d450393073ba17172f74276965
kyrylsherstobaiev <novemberua@gmail.com>
2021-03-15 17:03:40 +0200

    correct universal loop

--- homework13_2/script.js
+++ homework13_2/script.js
@@ -4,61 +4,36 @@ let inputArray = [];
 
 //***************************************************************/
 
-inputs.forEach(function (input) {
-	input.addEventListener('change', getValueInput);
-	input.addEventListener('focus', getClearValue);
+inputs.forEach(function (input, index) {
+	input.addEventListener('change', () => {
+		inputArray[index] = input.value;
+		textarea.innerHTML = inputArray.join(',');
+	});
+	input.addEventListener('focus', () => {
+		input.value = '';
+	});
 });
 
-function getClearValue() {
-	this.value = '';
-}
-
-function getValueInput() {
-	switch (this) {
-		case document.querySelector('input[data-text="text"]:first-of-type'):
-			inputArray[0] = this.value;
-			break;
-		case document.querySelector('input[data-text="text"]:nth-of-type(2)'):
-			inputArray[1] = this.value;
-			break;
-		case document.querySelector('input[data-text="text"]:last-of-type'):
-			inputArray[2] = this.value;
-			break;
-	}
-	textarea.innerHTML = inputArray.join(',');
-}
-
-//************************************************************** */
-
-
-//function getValueInput() {
-//	if (this == document.querySelector('input[data-text="text"]:first-of-type')) {
-//		inputArray[0] = this.value;
-//	}
-//	if (this == document.querySelector('input[data-text="text"]:nth-of-type(2)')) {
-//		inputArray[1] = this.value;
-//	}
-//	if (this == document.querySelector('input[data-text="text"]:last-of-type')) {
-//		inputArray[2] = this.value;
-//	}
-//	textarea.innerHTML = inputArray.join(',');
-//	console.log(inputArray);
-//}
-
 //***************************************************************/
-
-//inputs.forEach(function (input) {
-//	input.addEventListener('change', getValueInput)
-//	input.addEventListener('focus', getClearValue)
-//});
 
 //function getClearValue() {
 //	this.value = '';
 //}
 
 //function getValueInput() {
-//	inputArray.push(this.value);
+//	switch (this) {
+//		case document.querySelector('input[data-text="text"]:first-of-type'):
+//			inputArray[0] = this.value;
+//			break;
+//		case document.querySelector('input[data-text="text"]:nth-of-type(2)'):
+//			inputArray[1] = this.value;
+//			break;
+//		case document.querySelector('input[data-text="text"]:last-of-type'):
+//			inputArray[2] = this.value;
+//			break;
+//	}
 //	textarea.innerHTML = inputArray.join(',');
 //}
 
-//console.dir(inputArray);
+//***************************************************************/
+
